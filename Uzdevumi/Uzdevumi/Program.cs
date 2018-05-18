0db39def37154a161ac6c4317fb5edd47023246c
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-18 21:16:34 +0300

    1_summit

---
+++ Uzdevumi/Uzdevumi/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzdevumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int izvele = 1;
+            ReizDal RZ = new ReizDal();
+            
+            while (izvele != 0)
+            {
+                Console.WriteLine("\n" + "\t" + "1- Saskaitīšana");
+                Console.WriteLine("\t" + "2- Atņemšana");
+                Console.WriteLine("\t" + "3- Reizināšana");
+                Console.WriteLine("\t" + "4- Dalīšana");
+
+                Console.WriteLine("\t" + "0- Iziet\n");
+
+                string input = Console.ReadLine();
+                izvele = Convert.ToInt16(input);
+                //int rezult = Funkcija1();
+                //Console.WriteLine(rezult);
+
+                switch (izvele)
+                {
+                    case 1:
+
+                        int c = Funkcija1();
+                        Console.WriteLine("\t Iznākums = " + c);
+                        break;
+                    case 2:
+                        int c2 = Funkcija2();
+                        Console.WriteLine("\t Iznākums = " + c2);
+                        break;
+                    case 3:
+                        double c3 = RZ.Funkcija3();
+                        Console.WriteLine("\t Iznākums = " + c3);
+                        break;
+                    case 4:
+                        double c4 = RZ.Funkcija4();
+                        Console.WriteLine("\t Iznākums = " + c4);
+                        break;
+
+                    default:
+                        Console.WriteLine("Ievade nav pareiza \n");
+                        break;
+                }
+
+            }
+
+        }
+        static int Funkcija1()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli : ");
+            string skaitlis1 = Console.ReadLine();
+            int a = Convert.ToInt16(skaitlis1);
+
+            Console.WriteLine("Ievadiet otro skaitli : ");
+            string skaitlis2 = Console.ReadLine();
+            int b = Convert.ToInt16(skaitlis2);
+
+
+            int c = a + b;
+            return c;
+        }
+        static int Funkcija2()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli : ");
+            string skaitlis1 = Console.ReadLine();
+            int a = Convert.ToInt16(skaitlis1);
+
+            Console.WriteLine("Ievadiet otro skaitli : ");
+            string skaitlis2 = Console.ReadLine();
+            int b = Convert.ToInt16(skaitlis2);
+
+
+            int c2 = a - b;
+            return c2;
+
+
+        }
+        
+    }   
+        
+    
+}
---
+++ Uzdevumi/Uzdevumi/ReizDal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzdevumi
+{
+    class ReizDal
+    {
+
+
+        public double Funkcija3()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli : ");
+            string skaitlis1 = Console.ReadLine();
+            double a3 = Convert.ToDouble(skaitlis1);
+
+            Console.WriteLine("Ievadiet otro skaitli : ");
+            string skaitlis2 = Console.ReadLine();
+            double b3 = Convert.ToDouble(skaitlis2);
+
+            double c3 = a3 * b3;
+            return c3;
+        }
+
+    
+        public double Funkcija4()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli : ");
+            string skaitlis1 = Console.ReadLine();
+            double a4 = Convert.ToDouble(skaitlis1);
+
+            Console.WriteLine("Ievadiet otro skaitli : ");
+            string skaitlis2 = Console.ReadLine();
+            double b4 = Convert.ToDouble(skaitlis2);
+
+            double c4 = a4 / b4;
+            return c4;
+        }
+
+
+
+
+
+
+    }
+}
